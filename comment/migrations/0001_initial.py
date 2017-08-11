f2e95f7c0c31bde05615f20ac064cd3e2ad4b505
Will Skywalker <cxbats@gmail.com>
2017-08-11 17:17:47 +0800

    Fixed pep8 issues

--- comment/migrations/0001_initial.py
+++ comment/migrations/0001_initial.py
@@ -10,10 +10,6 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
-    ]
-
     operations = [
         migrations.CreateModel(
             name='Comment',
